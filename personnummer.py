1588563bd29c266ddff230f7495a8f1f8d8f84ff
Soma Szabó <szabosonka@gmail.com>
2022-04-18 17:37:07 +0200

    Added File Reading Functionality

--- personnummer.py
+++ personnummer.py
@@ -1,5 +1,5 @@
 from os import listdir, name, system
-from os.path import isfile, join
+from os.path import isfile, join, exists
 from datetime import date, datetime
 
 
@@ -36,7 +36,7 @@ class IdNumber:
         else:
             return False
 
-    def print_ID(self, **err: str):
+    def print_ID(self, err: str = ""):
         print(f"{self.year+self.month+self.day+self.number}     {err}")
 
     def get_year(self):
@@ -50,6 +50,9 @@ class IdNumber:
 
     def get_number(self):
         return self.number
+
+    def get_ID(self):
+        return self.year+self.month+self.day+self.number
 
 
 def validate_format(inp: str):
@@ -86,39 +89,98 @@ def create_ID_object(id_number: str):
     return id
 
 
-def validate_ID_number(inp):
-    if validate_format():
-        standrad_inp = convert_input_to_standrad_format(inp)
-        if len(standrad_inp) == 10:
-            if standrad_inp.isdecimal():
-                id = create_ID_object(standrad_inp)
-                if id.valid_date() != True:
-                    id.print_ID("Invalid date")
-                elif id.checksum() != True:
-                    id.print_ID("Incorrect checksum")
+def validate_ID_number(inp: str, file: str = "UserInputs.txt"):
+    err = ""
+    if len(inp) > 7:
+        if validate_format(inp):
+            standrad_inp = convert_input_to_standrad_format(inp)
+            if len(standrad_inp) == 10:
+                if standrad_inp.isdecimal():
+                    id = create_ID_object(standrad_inp)
+                    if id.valid_date() != True:
+                        id.print_ID("Invalid date")
+                        err = "Invalid date"
+                        with open("ValidatedInputs/InvalidIDs.txt", "a") as f:
+                            f.write(f"{inp}     {err}\n")
+
+                    elif id.checksum() != True:
+                        id.print_ID("Incorrect checksum")
+                        err = "Incorrect checksum"
+                        with open("ValidatedInputs/InvalidIDs.txt", "a") as f:
+                            f.write(f"{inp}     {err}\n")
+
+                    else:
+                        id.print_ID()
+                        valids = []
+                        with open("ValidatedInputs/ValidIDs.txt", "r") as f:
+                            for i in f:
+                                valids.append(i.strip())
+
+                        for i in valids:
+                            if convert_input_to_standrad_format(i) == id.get_ID():
+                                return
+                        with open("ValidatedInputs/ValidIDs.txt", "a") as f:
+                            f.write(f"{inp}\n")
+
                 else:
-                    id.print_ID()
+                    print(f"{standrad_inp}    Contains non-numeric characters")
+                    err = "Contains non-numeric characters"
+                    with open("ValidatedInputs/InvalidIDs.txt", "a") as f:
+                        f.write(f"{inp}     {err}\n")
+
+            elif len(standrad_inp) > 10:
+                print(f"{standrad_inp}   Too long")
+                err = "Too long"
+                with open("ValidatedInputs/InvalidIDs.txt", "a") as f:
+                    f.write(f"{inp}     {err}\n")
 
             else:
-                print(f"{inp}    Contain non-numeric characters")
-        elif len(inp) > 10:
-            print(f"{inp}   Too long")
+                print(f"{standrad_inp}   Too short")
+                err = "Too short"
+                with open("ValidatedInputs/InvalidIDs.txt", "a") as f:
+                    f.write(f"{inp}     {err}\n")
+
         else:
-            print(f"{inp}   Too short")
+            print(f"{inp}   Invalid format")
+            print("""
+            Valid formats:
+                620129-8558
+                62 01 29 8558
+                1962-01-29 8558
+                6201298558
+
+            """)
+            err = "Invalid format"
+            with open("ValidatedInputs/InvalidIDs.txt", "a") as f:
+                f.write(f"{inp}     {err}\n")
     else:
-        print(f"{inp}   Invalid format")
-        print(""" 
-        Valid formats:
-            620129-8558
-            62 01 29 8558
-            1962-01-29 8558
-            6201298558
-        
-        """)
+        print(f"{inp}   Too short")
+        err = "Too short"
+        with open("ValidatedInputs/InvalidIDs.txt", "a") as f:
+            f.write(f"{inp}     {err}\n")
+    file_exists = exists(f"ValidatedInputs/{file}")
+    if file_exists:
+        with open(f"ValidatedInputs/{file}", "a") as f:
+            f.write(f"{inp}     {err}\n")
+    else:
+        with open(f"ValidatedInputs/{file}", "x") as f:
+            f.write(f"{inp}     {err}\n")
 
 
 def read_ID_num_from_file():
-    print("File read function will be here")
+    onlyfiles = [f for f in listdir(
+        "InputsToRead") if isfile(join("InputsToRead", f))]
+    print("Which file would you like to read from?")
+    print("     ", end="")
+    for i in range(len(onlyfiles)):
+        print(f"({i+1}){onlyfiles[i]}", end="")
+    print(f"     ({len(onlyfiles)+1})Exit")
+    inp = int(input("Ans: "))
+    if inp == len(onlyfiles)+1:
+        return
+    with open(f"InputsToRead/{onlyfiles[inp-1]}", "r") as f:
+        for i in f:
+            validate_ID_number(i.strip(), onlyfiles[inp-1])
 
 
 def clearConsole(): return system('cls' if name in ('nt', 'dos') else 'clear')
@@ -127,7 +189,7 @@ def clearConsole(): return system('cls' if name in ('nt', 'dos') else 'clear')
 def main():
     while True:
         inp = input("""Would you like to:
-            (1)Input an ID number       (2)Read ID number from a file       (3)Stop the programme
+            (1)Input an ID number       (2)Read ID number for validation       (3)Read validated ID numbers       (4)Stop the programme
 Ans: """)
         if inp == "1":
             clearConsole()
@@ -139,11 +201,13 @@ Ans: """)
             clearConsole()
             read_ID_num_from_file()
         elif inp == "3":
+            pass
+        elif inp == "4":
             clearConsole()
             print("Good bye!")
             break
         else:
-            print("You need to answer with either 1 or 2")
+            print("You need to answer with either 1, 2 or 3")
 
 
 if __name__ == "__main__":
