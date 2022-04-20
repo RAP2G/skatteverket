0c1044b26e364286427dea60ceb18dbfb6c1eb68
Soma Szabó <szabosonka@gmail.com>
2022-04-20 15:08:51 +0200

    Fixd no duplication

--- personnummer.py
+++ personnummer.py
@@ -1,3 +1,4 @@
+from msilib.schema import File
 from os import listdir, name, system
 from os.path import isfile, join, exists
 from datetime import date, datetime
@@ -55,6 +56,9 @@ class IdNumber:
         return self.year+self.month+self.day+self.number
 
 
+def clearConsole(): return system('cls' if name in ('nt', 'dos') else 'clear')
+
+
 def validate_format(inp: str):
     if "-" == inp[6]:
         return True
@@ -89,12 +93,17 @@ def create_ID_object(id_number: str):
     return id
 
 
-def write_invalid(inp, err):
+def write_invalid(inp: str, err, invalids: list):
+    file_exists = exists("ValidatedInputs/InvalidIDs.txt")
+    if file_exists:
+        for i in invalids:
+            if inp in i:
+                return
     with open("ValidatedInputs/InvalidIDs.txt", "a") as f:
         f.write(f"{inp}     {err}\n")
 
 
-def validate_ID_number(inp: str, file: str = "UserInputs.txt"):
+def validate_ID_number(inp: str, valids: list, invalids: list, file: str = "UserInputs.txt", allinputs: list = []):
     err = ""
     if len(inp) > 7:
         if validate_format(inp):
@@ -105,56 +114,51 @@ def validate_ID_number(inp: str, file: str = "UserInputs.txt"):
                     if id.valid_date() != True:
                         id.print_ID("Invalid date")
                         err = "Invalid date"
-                        write_invalid(inp, err)
+                        write_invalid(inp, err, invalids)
                     elif id.checksum() != True:
                         id.print_ID("Incorrect checksum")
                         err = "Incorrect checksum"
-                        write_invalid(inp, err)
+                        write_invalid(inp, err, invalids)
                     else:
                         id.print_ID()
-                        valids = []
-                        with open("ValidatedInputs/ValidIDs.txt", "r") as f:
-                            for i in f:
-                                valids.append(i.strip())
                         for i in valids:
                             if convert_input_to_standrad_format(i) == id.get_ID():
-                                return
-                        with open("ValidatedInputs/ValidIDs.txt", "a") as f:
-                            f.write(f"{inp}\n")
+                                break
+                        else:
+                            with open("ValidatedInputs/ValidIDs.txt", "a") as f:
+                                f.write(f"{inp}\n")
 
                 else:
                     print(f"{standrad_inp}    Contains non-numeric characters")
                     err = "Contains non-numeric characters"
-                    write_invalid(inp, err)
+                    write_invalid(inp, err, invalids)
 
             elif len(standrad_inp) > 10:
                 print(f"{standrad_inp}   Too long")
                 err = "Too long"
-                write_invalid(inp, err)
+                write_invalid(inp, err, invalids)
 
             else:
                 print(f"{standrad_inp}   Too short")
                 err = "Too short"
-                write_invalid(inp, err)
+                write_invalid(inp, err, invalids)
 
         else:
             print(f"{inp}   Invalid format")
             err = "Invalid format"
-            write_invalid(inp, err)
+            write_invalid(inp, err, invalids)
     else:
         print(f"{inp}   Too short")
         err = "Too short"
-        write_invalid(inp, err)
-    file_exists = exists(f"ValidatedInputs/{file}")
-    if file_exists:
-        with open(f"ValidatedInputs/{file}", "a") as f:
-            f.write(f"{inp}     {err}\n")
-    else:
-        with open(f"ValidatedInputs/{file}", "x") as f:
-            f.write(f"{inp}     {err}\n")
+        write_invalid(inp, err, invalids)
+    for i in allinputs:
+        if inp in i:
+            return
+    with open(f"ValidatedInputs/{file}", "a") as f:
+        f.write(f"{inp}     {err}\n")
 
 
-def read_ID_num_from_file():
+def read_ID_num_from_file(valids: list, invalids: list, allinputs: list):
     onlyfiles = [f for f in listdir(
         "InputsToRead") if isfile(join("InputsToRead", f))]
     print("Which file would you like to read from?")
@@ -167,7 +171,10 @@ def read_ID_num_from_file():
         return
     with open(f"InputsToRead/{onlyfiles[inp-1]}", "r") as f:
         for i in f:
-            validate_ID_number(i.strip(), onlyfiles[inp-1])
+            validate_ID_number(i.strip(), valids, invalids,
+                               "AllValidatedtextInputs.txt", allinputs)
+    input()
+    clearConsole()
 
 
 def read_validated_ID():
@@ -179,28 +186,53 @@ def read_validated_ID():
         print(f"({i+1}){onlyfiles[i]}    ", end="")
     print(f"     ({len(onlyfiles)+1})Exit")
     inp = int(input("Ans: "))
+    if inp == len(onlyfiles)+1:
+        return
     with open(f"ValidatedInputs/{onlyfiles[inp-1]}", "r") as f:
         for i in f:
             print(i.strip())
+    input()
+    clearConsole()
 
 
-def clearConsole(): return system('cls' if name in ('nt', 'dos') else 'clear')
+def user_input(inp: str, valids: list, invalids: list):
+    print("Please input an ID number")
+    inp = input("Ans: ")
+    validate_ID_number(inp, valids, invalids)
+    clearConsole()
 
 
 def main():
+    valids = []
+    invalids = []
+    allinputs = []
     while True:
+        file_exists = exists("ValidatedInputs/ValidIDs.txt")
+        if file_exists:
+            with open("ValidatedInputs/ValidIDs.txt", "r") as f:
+                for i in f:
+                    valids.append(i.strip())
+        file_exists = exists("ValidatedInputs/InvalidIDs.txt")
+        if file_exists:
+            with open("ValidatedInputs/InvalidIDs.txt", "r") as f:
+                for i in f:
+                    invalids.append(i.strip())
+        file_exists = exists("ValidatedInputs/AllValidatedTextInputs.txt")
+        if file_exists:
+            with open("ValidatedInputs/AllValidatedTextInputs.txt", "r") as f:
+                for i in f:
+                    allinputs.append(i.strip())
+
         inp = input("""Would you like to:
             (1)Input an ID number       (2)Read ID number for validation       (3)Read validated ID numbers       (4)Stop the programme
 Ans: """)
         if inp == "1":
             clearConsole()
-            print("Please input an ID number")
-            inp = input("Ans: ")
-            validate_ID_number(inp)
-            print()
+            user_input()
+
         elif inp == "2":
             clearConsole()
-            read_ID_num_from_file()
+            read_ID_num_from_file(valids, invalids, allinputs)
         elif inp == "3":
             clearConsole()
             read_validated_ID()
@@ -214,3 +246,7 @@ Ans: """)
 
 if __name__ == "__main__":
     main()
+
+
+# TODO: How many numbers in pnr?
+# Any dash?
